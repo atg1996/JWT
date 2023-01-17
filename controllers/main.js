34986bb064abb8f7600e49ef02f5773ced4fea05
Tigran Asatryan <t.asatryan96@gmail.com>
2023-01-17 12:21:13 +0400

    jwt basics

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,36 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const CustomAPIError = require("../errors/custom-error")
+
 const login = async (req,res) => {
-    res.send("Fake Login/Register/Signup")
+try {
+    const {username,password} = req.body;
+
+    if(!username || !password) {
+        
+    throw new CustomAPIError("please provide login and password ", 400)
+    }
+    
+    //dummy Id 
+    const id = new Date().getDate();
+    const token = jwt.sign({ id, username }, String(process.env.JWT_SECRET), {
+        expiresIn: '30d',
+      })
+    
+      res.status(200).json({ msg: 'user created', token })
+}
+    catch (error){
+console.log(error);
+    }
+
 }
 
 const dashboard = async (req,res) => {
+
     const number = Math.floor(Math.random()*100);
     res.status(200).json({msg:`hello man`, secret:`your lucky number is ${number}`});
+
 }
 
 
